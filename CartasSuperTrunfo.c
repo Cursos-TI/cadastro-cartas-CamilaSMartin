b3368c5ff45b169c46d6ca7941a12e0d7540cd4e
CamilaSMartin <kamyllamartin14@gmail.com>
2026-04-23 20:56:15 +0000

    criado code cadastro cartas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,9 +7,75 @@
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
 
+  char carta[3];
+  char cidade[50];
+  char estado[80];
+  float pib, população, area, pontuação;
+
   // Área para entrada de dados
 
+  printf("Digite o código Carta: ");
+  scanf("%s", &carta);
+
+
+  printf("Digite o Estado: ");
+  scanf("%s", &estado);
+
+  printf("Digite a Cidade: ");
+  scanf("%s", &cidade);
+
+  printf("Digite a Área: ");
+  scanf("%f", &area);
+
+  printf("Digite a População: ");
+  scanf("%f", &população);
+
+  printf("Digite o PIB: ");
+  scanf("%f", &pib);
+
+  printf("Digite a Pontuação: ");
+  scanf("%f", &pontuação);
+
+  printf("Digite o Código Carta: ");
+  scanf("%s", &carta);
+
+  printf("Digite o Estado: ");
+  scanf("%s", &estado);
+
+  printf("Digite a Cidade: ");
+  scanf("%s", &cidade);
+
+  printf("Digite a Área: ");
+  scanf("%f", &area);
+
+  printf("Digite a População: ");
+  scanf("%f", &população);
+
+  printf("Digite o PIB: ");
+  scanf("%f", &pib);
+
+  printf("Digite a Pontuação: ");
+  scanf("%f", &pontuação);  
+
+   
+
   // Área para exibição dos dados da cidade
+
+   printf("Carta: %s\n", carta);
+   printf("Cidade: %s\n", cidade);
+   printf("Estado: %s\n", estado);
+   printf("População: %.3f\n", população);
+   printf("Área: %.3f km\n", area);
+   printf("PIB: %.3f\n", pib);
+   printf("Pontuação: %.1f\n", pontuação);
+   
+   printf("Carta: %s\n", carta);
+   printf("Cidade: %s\n", cidade);
+   printf("Estado: %s\n", estado);
+   printf("População: %.3f\n", população);
+   printf("Área: %.3f km\n", area);
+   printf("PIB: %.3f\n", pib);
+   printf("Pontuação: %.1f\n", pontuação);
 
 return 0;
 } 
